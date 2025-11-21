dde018566be0cfffd052b15475ff9759d09c25a2
shinoccarhuas15 <juniorquispeccarhuas15@gmail.com>
2025-11-21 09:10:23 -0500

    E cambiado mi views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.urls import path
+from . import views
 
-# Create your views here.
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.lista_reportes, name='lista_reportes'),
+    path('crear/', views.crear_reporte, name='crear_reporte'),
+    path('editar/<int:pk>/', views.editar_reporte, name='editar_reporte'),
+    path('eliminar/<int:pk>/', views.eliminar_reporte, name='eliminar_reporte'),
+]
